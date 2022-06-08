dd7215087840ef5a7a7d8aa3300ae9e248160aee
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2022-06-08 21:07:36 +0800

    Initial Commit

---
+++ ScanningDataCamera/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ScanningDataCamera
+//
+//  Created by Runhua Huang on 2022/6/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScanningDataCamera/ScanningDataCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanningDataCameraApp.swift
+//  ScanningDataCamera
+//
+//  Created by Runhua Huang on 2022/6/8.
+//
+
+import SwiftUI
+
+@main
+struct ScanningDataCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
